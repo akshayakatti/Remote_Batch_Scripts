bd20b874c14cc0ff957234c3b6accee65f0f0f00
Akshaya A K <38110588+akshayakatti@users.noreply.github.com>
2020-09-16 17:39:59 +0530

    Update remote_batch_script_scheduletask.bat

--- remote_batch_script_scheduletask.bat
+++ remote_batch_script_scheduletask.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-schtasks /Create /SC DAILY /TN "PRT" /TR "'C:\Program Files\Internet Explorer\iexplore.exe' \"https://inside-prime.bosch.com/PWA/RBEI/_layouts/15/pwa/timesheet/mytssummary.aspx"" /ST "17:30"
+schtasks /Create /SC DAILY /TN "PRT" /TR "'C:\Program Files\Internet Explorer\iexplore.exe' \"https://www.facebook.com/"" /ST "17:30"
